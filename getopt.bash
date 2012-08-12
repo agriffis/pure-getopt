7c5ed8c232dc0aa982208e333c1eb5ed6ffa79c2
Aron Griffis <aron@arongriffis.com>
2012-08-12 12:53:03 -0400

    Add an inner _getopt() function in prep for more _getopt_*() helpers. Use a shiftwidth of 2 since this will get deep otherwise.

--- getopt.bash
+++ getopt.bash
@@ -18,83 +18,89 @@
 #  * full list of differences between this and GNU getopt (should be short)
 
 getopt() {
+  _getopt() {
     if [[ "$1 $3 $5 $7" != "-o --long -n --" ]]; then
-        echo "Assertion failed: getopt call changed." >&2
-        return 1
+      echo "Assertion failed: getopt call changed." >&2
+      return 1
     fi
 
     declare short="$2" long="$4" name="$6" opts=() params=() o
     set -- "${@:8}"
 
     while [[ $# -gt 0 ]]; do
-        case $1 in
-            (--)
-                params+=( "${@:2}" )
-                break ;;
+      case $1 in
+        (--)
+          params+=( "${@:2}" )
+          break ;;
 
-            (--*=*)
-                o=${1%%=*}
-                if [[ ,"$long", == *,"${o#--}":,* ]]; then
-                    opts+=( "$o" "${o#*=}" )
-                elif [[ ,"$long", == *,"${o#--}",* ]]; then
-                    echo "$name: option '$o' doesn't allow an argument" >&2
-                    return 1
-                else
-                    echo "$name: unrecognized option '$o'" >&2
-                    return 1
-                fi ;;
+        (--*=*)
+          o=${1%%=*}
+          if [[ ,"$long", == *,"${o#--}":,* ]]; then
+            opts+=( "$o" "${o#*=}" )
+          elif [[ ,"$long", == *,"${o#--}",* ]]; then
+            echo "$name: option '$o' doesn't allow an argument" >&2
+            return 1
+          else
+            echo "$name: unrecognized option '$o'" >&2
+            return 1
+          fi ;;
 
-            (--?*)
-                o=$1
-                if [[ ,"$long", == *,"${o#--}",* ]]; then
-                    opts+=( "$o" )
-                elif [[ ,"$long", == *,"${o#--}:",* ]]; then
-                    if [[ $# -ge 2 ]]; then
-                        shift
-                        opts+=( "$o" "$1" )
-                    else
-                        echo "$name: option '$o' requires an argument" >&2
-                        return 1
-                    fi
-                else
-                    echo "$name: unrecognized option '$o'" >&2
-                    return 1
-                fi ;;
+        (--?*)
+          o=$1
+          if [[ ,"$long", == *,"${o#--}",* ]]; then
+            opts+=( "$o" )
+          elif [[ ,"$long", == *,"${o#--}:",* ]]; then
+            if [[ $# -ge 2 ]]; then
+              shift
+              opts+=( "$o" "$1" )
+            else
+              echo "$name: option '$o' requires an argument" >&2
+              return 1
+            fi
+          else
+            echo "$name: unrecognized option '$o'" >&2
+            return 1
+          fi ;;
 
-            (-*)
-                o=${1::2}
-                if [[ "$short" == *"${o#-}":* ]]; then
-                    if [[ ${#1} -gt 2 ]]; then
-                        opts+=( "$o" "${1:2}" )
-                    elif [[ $# -ge 2 ]]; then
-                        shift
-                        opts+=( "$o" "$1" )
-                    else
-                        echo "$name: option '$o' requires an argument" >&2
-                    fi
-                elif [[ "$short" == *"${o#-}"* ]]; then
-                    opts+=( "$o" )
-                    if [[ ${#1} -gt 2 ]]; then
-                        set -- "$o" "-${1:2}" "${@:2}"
-                    fi
-                else
-                    echo "$name: unrecognized option '$o'" >&2
-                    return 1
-                fi ;;
+        (-*)
+          o=${1::2}
+          if [[ "$short" == *"${o#-}":* ]]; then
+            if [[ ${#1} -gt 2 ]]; then
+              opts+=( "$o" "${1:2}" )
+            elif [[ $# -ge 2 ]]; then
+              shift
+              opts+=( "$o" "$1" )
+            else
+              echo "$name: option '$o' requires an argument" >&2
+            fi
+          elif [[ "$short" == *"${o#-}"* ]]; then
+            opts+=( "$o" )
+            if [[ ${#1} -gt 2 ]]; then
+              set -- "$o" "-${1:2}" "${@:2}"
+            fi
+          else
+            echo "$name: unrecognized option '$o'" >&2
+            return 1
+          fi ;;
 
-            (*)
-                params+=( "$1" ) ;;
-        esac
+        (*)
+          params+=( "$1" ) ;;
+      esac
 
-        shift
+      shift
     done
 
     if [[ ${#opts[@]} -gt 0 ]]; then
-        printf '%q ' "${opts[@]}"
+      printf '%q ' "${opts[@]}"
     fi
     printf '%s' '--'
     if [[ ${#params[@]} -gt 0 ]]; then
-        printf ' %q' "${params[@]}"
+      printf ' %q' "${params[@]}"
     fi
     echo
+  }
+
+  _getopt "$@"
 }
+
+# vim:sw=2
